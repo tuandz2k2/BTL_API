55a13ef0ad1b455a4ec652329019bdd055029faf
BabiPunSociu <Nguyendung281002@gmail.com>
2023-04-21 11:56:02 +0700

    DŨng 21/4 có controller mới

---
+++ Controllers/ThoiTiet.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace BTL_api.Controllers
+{
+
+    public class ThoiTiet : Controller
+    {
+        public ActionResult Index(string place)
+        {
+            ViewBag.Place = place;
+            return View();
+        }
+
+        public ActionResult TheoGio(string place)
+        {
+            ViewBag.Place = place;
+            return View();
+        }
+        public ActionResult NgayMai(string place)
+        {
+            ViewBag.Place = place;
+            return View();
+        }
+        public ActionResult BaNgayToi(string place)
+        {
+            ViewBag.Place = place;
+            return View();
+        }
+        public ActionResult NamNgayToi(string place)
+        {
+            ViewBag.Place = place;
+            return View();
+        }
+        public ActionResult MuoiNgayToi(string place)
+        {
+            ViewBag.Place = place;
+            return View();
+        }
+    }
+}
--- resource/js/site.js
+++ resource/js/site.js
@@ -1,3 +1,4 @@
+
 function getEmbedUrl() {
     let tinhThanh = $('input[name=TinhThanh]').val();
     if (!tinhThanh) {
@@ -27,34 +28,69 @@ function reRenderWidget() {
     $('.widget-container').html(htmlData);
 }
 
+// Khai báo hàm để khởi tạo tính năng tìm kiếm location
 function initSearchWidget() {
+    /*
+    Áp dụng plugin jQuery UI Autocomplete cho các phần tử HTML có
+        class ".search-location"
+    */
     $(".search-location").autocomplete({
-            minLength: 0,
-            source: function(request, response) {
-                $.ajax({
-                    url: "/api/Catalog/search-location-widget",
-                    type: 'GET',
-                    dataType: "json",
-                    data: {
-                        q: request.term
-                    },
-                    success: function(data) {
-                        response(data);
+        /*
+        Thiết lập độ dài tối thiểu của chuỗi input là 0
+        => Sẽ hiển thị gợi ý ngay khi người dùng bắt đầu nhập
+        */
+        minLength: 0,
+
+        /*
+        Xử lý dữ liệu nguồn để gợi ý cho autocomplete:
+        Nó thực hiện một yêu cầu AJAX đến url với dữ liệu truyền lên là giá trị
+            của chuỗi tìm kiếm: request.term
+            => Xử lý kết quả thành JSON: response(data)
+        */
+        source: function (request, response) {
+            $.ajax({
+                // Truyền vào chuỗi tìm kiếm
+                url: "https://api.mapbox.com/geocoding/v5/mapbox.places/"
+                    + request.term
+                    + ".json?access_token=pk.eyJ1IjoiYmFiaXB1bnNvY2l1IiwiYSI6ImNsZnFvZnNpazAwZ24zeXFtZGw3am5xNzUifQ.kPyybZgMT0bhvmIKSgyqKA",
+                type: 'GET',
+                dataType: "json",
+                lang: 'vi',
+                success: function (data) {
+                    var cities = [];
+                    for (var i = 0; i < data.features.length; i++) {
+                        /*
+                        if (data.features[i].place_type[0] === 'place') {
+                            cities.push(data.features[i].text);
+                        }
+                        */
+                        cities.push(data.features[i].text);
                     }
-                });
-            },
-            focus: function(event, ui) {
-                $(".search-location").val(ui.item.text);
-                $('input[name=TinhThanh]').val(ui.item.value)
-                return false;
-            },
-            select: function(event, ui) {
-                fillEmbedUrl()
-                reRenderWidget()
-                return false;
-            }
-        })
-        .autocomplete("instance")._renderItem = function(ul, item) {
+                    response(cities);
+                },
+                error: function (xhr, textStatus, errorThrown) {
+                    console.error('Lỗi khi gọi API MapBox: ' + textStatus, errorThrown);
+                }
+            });
+        },
+        /* Cập nhật giá trị của phần tử  */
+        focus: function (event, ui) {
+            $(".search-location").val(ui.item.text);
+            $('input[name=TinhThanh]').val(ui.item.value)
+            return false;
+        },
+        /* Hàm callback được gọi khi một mục được chọn từ gợi ý */
+        select: function (event, ui) {
+            fillbedUrl()
+            reRenderWidget()
+            return false;
+        }
+    })
+        /* Tạo cách hiển thị gợi ý mặc định của Autocomlete:
+                - Tạo thẻ <li> với nội dung item.text
+                - Add thẻ <li> vào <ul> để hiển thị
+            */
+        .autocomplete("instance")._renderItem = function (ul, item) {
             return $("<li>")
                 .append("<span class='dropdown-item'>" + item.text + "</span>")
                 .appendTo(ul);
@@ -62,7 +98,8 @@ function initSearchWidget() {
 
 }
 
-$(document).ready(function() {
+/* Hàm sử lý sự kiện khi DOM được tải lên hoàn tất */
+$(document).ready(function () {
     //$('#carousel-ngay-toi').on('slide.bs.carousel', function (e) {
     //    var $e = $(e.relatedTarget);
     //    var idx = $e.index();
@@ -88,38 +125,61 @@ $(document).ready(function() {
     //    touch: true
     //});
 
+    //======================================================================
+    //      Xử lý search thành phố
+    // API MapBox để tìm thành phố
     $(".search-auto").autocomplete({
-            minLength: 0,
-            source: function(request, response) {
-                $.ajax({
-                    url: "/api/Catalog/search-location",
-                    type: 'GET',
-                    dataType: "json",
-                    data: {
-                        q: request.term
-                    },
-                    success: function(data) {
-                        response(data);
+        minLength: 0,
+        source: function (request, response) {
+            $.ajax({
+                // Truyền vào chuỗi tìm kiếm
+                url: "https://api.mapbox.com/geocoding/v5/mapbox.places/"
+                    + request.term
+                    + ".json?access_token=pk.eyJ1IjoiYmFiaXB1bnNvY2l1IiwiYSI6ImNsZnFvZnNpazAwZ24zeXFtZGw3am5xNzUifQ.kPyybZgMT0bhvmIKSgyqKA&types=place",
+                type: 'GET',
+                dataType: "json",
+                lang: 'vi',
+                success: function (data) {
+                    console.log(data);
+                    /*
+                    var cities = [];
+                    for (var i = 0; i < data.features.length; i++) {
+                        var x = data.features[i].text;
+                        cities.push(x);
                     }
-                });
-            },
-            focus: function(event, ui) {
-                $("#searchInput").val(ui.item.description);
-                $("#searchValue").val(ui.item.value);
-                return false;
-            },
-            select: function(event, ui) {
-                window.location.href = ui.item.value;
-                return false;
-            }
-        })
-        .autocomplete("instance")._renderItem = function(ul, item) {
+                    console.log("List city: " + cities);
+                    response(cities);
+                    */
+                    response(data.features);
+                },
+                error: function (xhr, textStatus, errorThrown) {
+                    console.error('Lỗi khi gọi API MapBox: ' + textStatus, errorThrown);
+                }
+            });
+        },
+        focus: function (event, ui) {
+            $("#searchInput").val(ui.item.place_name);
+            $("#searchValue").val(ui.item.value);
+            return false;
+        },
+        select: function (event, ui) {
+            window.location.href = ui.item.value;
+            return false;
+        }
+    })
+        .autocomplete("instance")._renderItem = function (ul, item) {
             return $("<li>")
-                .append("<span class='dropdown-item'>" + item.text + "</span>")
+                .append("<span class='ui-menu-item'>" + item.place_name + "</span>")
                 .appendTo(ul);
         };
 
-    $('.frmSearch').submit(function(evt) {
+    /*
+    Xử lý sự kiện form submit:
+    - Ngăn chặn hành vi mặc định của form: evt.preventDefault()
+    - Lấy giá trị của trường có id="searchValue"
+    - Chuyển hướng đến URL tương ứng nếu có giá trị hợp lệ
+    */
+    $('.frmSearch').submit(function (evt) {
         evt.preventDefault();
         const url = $('#searchValue').val()
         if (url && url.trim().length > 0) {
@@ -128,7 +188,7 @@ $(document).ready(function() {
     })
 
 
-    $('#showMoreContent').click(function(evt) {
+    $('#showMoreContent').click(function (evt) {
         evt.preventDefault();
         if ($('.cover-content').hasClass('active')) {
             $('.cover-content').removeClass('active')
@@ -138,7 +198,7 @@ $(document).ready(function() {
             $('#showMoreContent').text('Ẩn bớt')
         }
     })
-    $('.showMoreContent').click(function(evt) {
+    $('.showMoreContent').click(function (evt) {
         evt.preventDefault();
         const target = $(this).data('target');
         if ($(target).hasClass('active')) {
@@ -149,7 +209,7 @@ $(document).ready(function() {
             $(this).text('Ẩn bớt')
         }
     })
-    $('.btn-toggle-toc').click(function(evt) {
+    $('.btn-toggle-toc').click(function (evt) {
         evt.preventDefault();
         $('.toc-links').toggle();
     })
